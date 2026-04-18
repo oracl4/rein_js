ebe76cc1fb546fc3878f6edee4bf8fd536445099
mahdiyusuf <it.mahdi.yusuf@gmail.com>
2026-04-18 18:07:14 +0800

    fix: Fixing package vulnerabilities

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const { get } = require('request')
+const axios = require('axios')
 
 const app = express()
 
@@ -35,22 +35,16 @@ app.listen(3000, () => console.log('Listening on port 3000!'))
 
 function request(url, returnBuffer = true, timeout = 10000) {
   return new Promise(function(resolve, reject) {
-    const options = Object.assign(
-      {},
-      {
-        url,
-        isBuffer: true,
-        timeout,
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-        }
-      },
-      returnBuffer ? { encoding: null } : {}
-    )
-
-    get(options, function(err, res) {
-      if (err) return reject(err)
-      return resolve(res)
+    axios.get(url, {
+      responseType: returnBuffer ? 'arraybuffer' : 'text',
+      timeout,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+      }
+    }).then(res => {
+      resolve({ body: res.data, headers: res.headers })
+    }).catch(err => {
+      reject(err)
     })
   })
 }
